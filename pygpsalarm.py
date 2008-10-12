8c6b8b936497943562b06ddcdb28a4268ec17564
Michał Rudowicz <blotko@gmail.com>
2008-10-12 16:44:54 +0200

    Changed licence to GPLv3 with permission of Jürgen Scheible.

--- pygpsalarm.py
+++ pygpsalarm.py
@@ -1,7 +1,21 @@
-# PyGPSAlarm v0.0.2
+#    PyGPSAlarm, script that wakes you up according to your GPS location.
+#    Copyright (C) 2008 Michał Rudowicz
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 # http://github.com/michalrud/pygpsalarm/
-# based on http://www.mobilenin.com/pys60/resources/bt_gps_reader.py
-
+# With permission by Jürgen Scheible this application includes some code  from his PyS60 online tutorial http://www.mobilenin.com/pys60/resources/bt_gps_reader.py .
 
 import appuifw, socket, e32, time
 
